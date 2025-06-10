9a99276da1794b8e6963c6f67bf5197d24b554e7
Jack <therealjackwu@gmail.com>
2025-06-10 04:20:22 -0700

    Added dense, mean-field, fixed cases

--- code/var_pt.py
+++ code/var_pt.py
@@ -5,6 +5,7 @@ warnings.filterwarnings("ignore")
 from scipy.interpolate import PchipInterpolator
 import pints
 import pints.toy
+from scipy.stats import multivariate_normal
 
 import nrpt
 
@@ -14,11 +15,15 @@ s = 0.7
 
 def RWMH_exploration_kernel(log_gamma, initial_x, num_iters):
     curr_point = initial_x
-    samples = np.zeros(num_iters)
+    d = 4
+    samples = np.zeros((num_iters, d))
     
     for i in range(num_iters):
-        new_point = np.random.normal(curr_point, s, 1)
+        new_point = np.zeros(d)
         
+        for j in range(d):
+            new_point[j] = np.random.normal(curr_point[j], s, 1)
+            
         p = min(1, np.exp(log_gamma(new_point)-log_gamma(curr_point)))
     
         if np.random.rand() < p:
@@ -76,18 +81,29 @@ def update_schedule(reject_rates, schedule):
     return new_schedule
 
     
-def update_reference(samples):
-    return (np.mean(samples), np.std(samples))
+def update_dense_reference(samples):
+    samples = np.asarray(samples)
+    return (np.mean(samples, axis=0), np.cov(samples.T))
+
+def update_diagonal_reference(samples):
+    samples = np.asarray(samples)
+    
+    variances = np.zeros(samples.shape[1])
+    for i in range(samples.shape[1]):
+        variances[i] = np.var(samples[:,i])
+        
+    return (np.mean(samples, axis=0), np.diag(variances))
 
 
-def vanilla_NRPT_with_RWMH(initial_state, betas, log_annealing_path, num_iterations):
+def vanilla_NRPT_with_RWMH(initial_state, betas, log_annealing_path, num_iterations, variation):
     num_distributions = len(betas)
+    d = 4
 
     samples = []
     reject_rates = np.zeros(num_distributions-1)
     
     x_at_tminus1 = initial_state.copy()
-    x_at_t = np.zeros(num_distributions)
+    x_at_t = np.zeros((num_distributions, d))
 
     for t in range(num_iterations):
         for init in range(num_distributions):
@@ -99,7 +115,11 @@ def vanilla_NRPT_with_RWMH(initial_state, betas, log_annealing_path, num_iterati
 
         for i in range(num_distributions-1):
             temp_alpha_vector[i] = nrpt.alpha(log_annealing_path[i], log_annealing_path[i+1], x_at_t[i], x_at_t[i+1])
-            reject_rates[i] = reject_rates[i] + (1 - temp_alpha_vector[i]) / 100
+            
+            if variation == False:
+                reject_rates[i] = reject_rates[i] + (1 - temp_alpha_vector[i]) / 1000
+            else:
+                reject_rates[i] = reject_rates[i] + (1 - temp_alpha_vector[i]) / 100
             
             if ((i%2==0 and t%2==0) or (i%2==1 and t%2==1)) and (np.random.rand() <= temp_alpha_vector[i]):
                 x_at_t[i], x_at_t[i+1] = x_at_t[i+1], x_at_t[i]
@@ -114,12 +134,12 @@ def vanilla_NRPT_with_RWMH(initial_state, betas, log_annealing_path, num_iterati
     
     
 ## Variational PT implementation!
-def variational_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_target, log_var_family, initial_phi):
+def variational_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_target, log_var_family, initial_phi, diagonal, variation):
     schedule = np.linspace(0, 1, num_chains)
     curr_state = initial_state
     curr_phi = initial_phi
     
-    Lambda_vs_r_points_VAR = []
+    Lambda_vs_r_points = []
     
     toReturn = []
     
@@ -131,7 +151,7 @@ def variational_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_t
         curr_var = log_var_family(curr_phi[0], curr_phi[1])
         path = nrpt.path(schedule, curr_var, log_target)
         
-        result = vanilla_NRPT_with_RWMH(curr_state, schedule, path, T)
+        result = vanilla_NRPT_with_RWMH(curr_state, schedule, path, T, variation)
         reject_rates = result["reject_rates"]
         samples = result["samples"]
         
@@ -139,19 +159,25 @@ def variational_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_t
             for chain in samples: toReturn.append(chain[-1])
         
         schedule = update_schedule(reject_rates, schedule)
-        curr_phi = update_reference([chain[-1] for chain in samples])
+        
+        if variation == True:
+            if diagonal == True:
+                curr_phi = update_diagonal_reference([chain[-1] for chain in samples])
+            else:
+                curr_phi = update_dense_reference([chain[-1] for chain in samples])
+            
         curr_state = samples[-1]
         
         print("Reject rates:",reject_rates)
         print("Schedule:",schedule)
         
         ## Collect points for Lambda vs. r plot
-        Lambda_vs_r_points_VAR.append([r, np.sum(reject_rates / (1 - reject_rates))])
+        Lambda_vs_r_points.append([r, np.sum(reject_rates / (1 - reject_rates))])
     
-    return toReturn, reject_rates, Lambda_vs_r_points_VAR
+    return toReturn, reject_rates, Lambda_vs_r_points
 
 
-## Kolmogorov-Smirnov test for kernel pi-invariance using N(0,1)
+### Kolmogorov-Smirnov test for kernel pi-invariance using N(0,1)
 # log_gamma = lambda x: -x**2 / 2
 # num_samples = 3000
 # iid_samples = np.random.normal(size=num_samples)
@@ -164,24 +190,45 @@ def variational_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_t
 # print("Kernel test p-value:", ks_result.pvalue)
 
 
-### Toy example
-# def log_var_family(mu, sigma):
-#     return lambda x: -0.5 * ((x-mu) / sigma)**2
+### Toy examples
+def log_var_family(mu, cov):
+    return lambda x: multivariate_normal.logpdf(np.array(x), mean=mu, cov=cov)
 
-# log_target = lambda x: -0.5 * ((x-10)/0.00001)**2  ## N(mu=10, std=0.00001) target
+d = 4
+mean = np.array([1,2,3,4])
+cov = np.array([
+    [1.0,  0.8,  0.5,  0.3],
+    [0.8,  1.5,  0.6,  0.4],
+    [0.5,  0.6,  2.0,  0.7],
+    [0.3,  0.4,  0.7,  1.8]
+])
+log_target = lambda x: multivariate_normal.logpdf(np.array(x), mean=mean, cov=cov)
 
-# num_chains = 15
-# initial_state = [0.1] * num_chains
-# num_tuning_rounds = 14
-# initial_phi = (0,0.5)
+num_chains = 15
+initial_state = [[0.25, 0.25, 0.25, 0.25]] * num_chains
+num_tuning_rounds = 10
+initial_phi = (np.zeros(d), np.eye(d))
 
-# samples, rates, Lambda_vs_r_points_VAR = variational_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_target, log_var_family, initial_phi)
-# print("Final reject rates:",rates)
-# print("The mean is:", np.mean(samples))
-# print("The var is:", np.var(samples))
+## Dense
+samples, rates, Lambda_vs_r_points_VAR_dense = variational_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_target, 
+                                                                        log_var_family, initial_phi, diagonal=False, variation=True)
+print("Final reject rates:",rates)
+print("(Dense Gaussian) Estimated mean:", np.mean(samples, axis=0))
+print("(Dense Gaussian) Estimated variance:", np.var(samples, axis=0))
+
+
+## Mean-field
+samples, rates, Lambda_vs_r_points_VAR_diagonal = variational_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_target, 
+                                                                           log_var_family, initial_phi, diagonal=True, variation=True)
+print("Final reject rates:",rates)
+print("(Mean-Field Gaussian) Estimated mean:", np.mean(samples, axis=0))
+print("(Mean-Field Gaussian) Estimated variance:", np.var(samples, axis=0))
         
+
+## Fixed N(0,I)
+samples, rates, Lambda_vs_r_points_fixed = variational_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_target,
+                                                                    log_var_family, initial_phi, diagonal=False, variation=False)
+print("Final reject rates:",rates)
+print("(Fixed Standard Gaussian) Estimated mean:", np.mean(samples, axis=0))
+print("(Fixed Standard Gaussian) Estimated variance:", np.var(samples, axis=0))
         
-        
-        
-        
-    
