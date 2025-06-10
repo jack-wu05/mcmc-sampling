061a5fd12243d958f1e1f9a31c818b06847bd3fc
Jack <therealjackwu@gmail.com>
2025-06-10 04:19:39 -0700

    Added KS test for Chow-Liu correctness, extended plotting

--- code/tree_pt.py
+++ code/tree_pt.py
@@ -56,7 +56,7 @@ def vanilla_NRPT_with_RWMH(initial_state, betas, log_annealing_path, num_iterati
 
         for i in range(num_distributions-1):
             temp_alpha_vector[i] = nrpt.alpha(log_annealing_path[i], log_annealing_path[i+1], x_at_t[i], x_at_t[i+1])
-            reject_rates[i] = reject_rates[i] + (1 - temp_alpha_vector[i]) / 1000
+            reject_rates[i] = reject_rates[i] + (1 - temp_alpha_vector[i]) / 10000
             
             if ((i%2==0 and t%2==0) or (i%2==1 and t%2==1)) and (np.random.rand() <= temp_alpha_vector[i]):
                 x_at_t[i], x_at_t[i+1] = x_at_t[i+1], x_at_t[i]
@@ -76,7 +76,7 @@ def tree_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_target):
     curr_state = initial_state
     
     d = 4
-    reference = lambda x: multivariate_normal.logpdf(np.array(x), mean=np.zeros(d), cov=np.identity(d))
+    reference = lambda x: multivariate_normal.logpdf(np.asarray(x), mean=np.zeros(d), cov=np.identity(d))
     
     Lambda_vs_r_points_TREE = []
     
@@ -111,43 +111,61 @@ def tree_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_target):
     return toReturn, reject_rates, Lambda_vs_r_points_TREE
 
 
-def plot(points1, points2):
+def plot(points1, points2, points3, points4):
     x1,y1 = zip(*points1)  
     x2,y2 = zip(*points2)
+    x3,y3 = zip(*points3)
+    x4,y4 = zip(*points4)
       
     plt.figure()
-    plt.plot(x1,y1, marker='o', color='b')
-    plt.plot(x2,y2, marker = 'o', color='r')
+    plt.plot(x1,y1, marker='o', color='b', label='Tree')
+    plt.plot(x2,y2, marker ='o', color='r', label='Dense Gaussian')
+    plt.plot(x3,y3, marker='o', color='g', label='Mean-Field Gaussian')
+    plt.plot(x4,y4, marker='o', color='c', label='Fixed N(0,I)')
+    plt.xlabel('Tuning round (r)')
+    plt.ylabel('GCB (Lambda)')
+    plt.legend()
     plt.grid(True)
     plt.show()
     
     
     
-## Kolmogorov-Smirnov test for correctness of Chow-Liu
-# log_gamma = lambda x: -x**2 / 2
-# num_samples = 3000
-# iid_samples = np.random.normal(size=num_samples)
+# ### Kolmogorov-Smirnov test for correctness of Chow-Liu
+# d = 4
+# num_samples = 2000
+# iid_data = gaussian_tree.generate_data1(num_samples)
 
-# kernel_samples = np.zeros(num_samples)
+# cl_tree = gaussian_tree.directed_graph(
+#                 gaussian_tree.tree_decomposition(iid_data)
+#             )    
+# tree_pdf = lambda x: gaussian_tree.tree_pdf(cl_tree, x)
+
+# kernel_samples = np.zeros((num_samples,d))
 # for i in range(num_samples):
-#     kernel_samples[i] = RWMH_exploration_kernel(log_gamma, iid_samples[i], 4000)[-1]
+#     kernel_samples[i] = RWMH_exploration_kernel(tree_pdf, iid_samples[i], 2000)[-1]
 
-# ks_result = ks_2samp(iid_samples, kernel_samples)
-# print("Kernel test p-value:", ks_result.pvalue)
+# ks_vector = np.zeros(d)
+# for j in range(kernel_samples.shape[1]):
+#     ks_result = ks_2samp(iid_samples[:,i], kernel_samples[:,i])
+#     ks_vector[j] = ks_result.pvalue
+    
+# print("KS variable-wise p-values:", ks_vector)
     
     
+
+
     
     
     
 ### Toy example
 d = 4
-mean = [1,2,3,4]
+mean = np.array([1,2,3,4])
 cov = np.array([
     [1.0,  0.8,  0.5,  0.3],
     [0.8,  1.5,  0.6,  0.4],
     [0.5,  0.6,  2.0,  0.7],
     [0.3,  0.4,  0.7,  1.8]
-])
+    ])
 log_target = lambda x: multivariate_normal.logpdf(np.array(x), mean=mean, cov=cov)
 
 samples = []
@@ -162,20 +180,11 @@ print()
 
 num_chains = 15
 initial_state = [[0.25, 0.25, 0.25, 0.25]] * num_chains
-num_tuning_rounds = 14
+num_tuning_rounds = 10
 samples, rates, Lambda_vs_r_points_TREE = tree_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_target)
 samples = np.array(samples)
-print("Estimated mean:", np.mean(samples, axis=0))
-print("Estimated variance:", np.var(samples, axis=0))
+print("(Tree) Estimated mean:", np.mean(samples, axis=0))
+print("(Tree) Estimated variance:", np.var(samples, axis=0))
 
-plot(Lambda_vs_r_points_TREE, Lambda_vs_r_points_VAR)
+plot(Lambda_vs_r_points_TREE, var_pt.Lambda_vs_r_points_VAR_dense, var_pt.Lambda_vs_r_points_VAR_diagonal, var_pt.Lambda_vs_r_points_fixed)
 
-
-
-
-
-
-
-        
-        
-    
