c87aa5ebb729b13b16667ea923b7c33985f11adc
Jack <therealjackwu@gmail.com>
2025-06-13 22:37:15 -0700

    Added colouring for restart rate tracking, extended plotting

--- code/tree_pt.py
+++ code/tree_pt.py
@@ -8,68 +8,36 @@ import var_pt
 import matplotlib.pyplot as plt
 
 
-def vanilla_NRPT_with_RWMH(initial_state, betas, log_annealing_path, num_iterations):
-    num_distributions = len(betas)
-    d = 4
-
-    samples = []
-    reject_rates = np.zeros(num_distributions-1)
-    
-    x_at_tminus1 = initial_state.copy()
-    x_at_t = np.zeros((num_distributions, d))
-
-    for t in range(num_iterations):
-        for init in range(num_distributions):
-            log_gamma = log_annealing_path[init]
-
-            x_at_t[init] = var_pt.RWMH_exploration_kernel(log_gamma, x_at_tminus1[init], 50)[-1]
-        
-        temp_alpha_vector = np.zeros(num_distributions)
-
-        for i in range(num_distributions-1):
-            temp_alpha_vector[i] = nrpt.alpha(log_annealing_path[i], log_annealing_path[i+1], x_at_t[i], x_at_t[i+1])
-            reject_rates[i] = reject_rates[i] + (1 - temp_alpha_vector[i]) / num_iterations
-            
-            if ((i%2==0 and t%2==0) or (i%2==1 and t%2==1)) and (np.random.rand() <= temp_alpha_vector[i]):
-                x_at_t[i], x_at_t[i+1] = x_at_t[i+1], x_at_t[i]
-
-        x_at_tminus1 = x_at_t.copy()
-        samples.append(x_at_tminus1.copy())
-
-    return {
-        "reject_rates": reject_rates,
-        "samples": samples
-    }
-    
-    
 ## Tree PT implementation!
 def tree_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_target):
     schedule = np.linspace(0, 1, num_chains)
-    curr_state = initial_state
+    curr_state = [(point, "g") for point in initial_state]
     
     d = 4
     reference = lambda x: multivariate_normal.logpdf(np.asarray(x), mean=np.zeros(d), cov=np.identity(d))
     
     Lambda_vs_r_points_TREE = []
+    RestartRate_vs_r_points = []
+    NumRestarts_vs_r_points = []
     
     toReturn = []
     for r in range(6, num_tuning_rounds):
         print("-----","TUNING ROUND", r,"-----")
         
+        curr_state = [(tup[0], "g") for tup in curr_state]
         T = 2**r
         
         path = nrpt.path(schedule, reference, log_target)
         
-        result = vanilla_NRPT_with_RWMH(curr_state, schedule, path, T)
+        restarts, result = var_pt.vanilla_NRPT_with_RWMH(curr_state, schedule, path, T)
         reject_rates = result["reject_rates"]
         samples = result["samples"]
         
-        for chain in samples: toReturn.append(chain[-1])
+        for chain in samples: toReturn.append(chain[-1][0])
        
-        cl_tree = gaussian_tree.directed_graph(
-                gaussian_tree.tree_decomposition([chain[-1] for chain in samples])
-                )    
-        reference = lambda x: gaussian_tree.tree_logpdf(cl_tree, x)
+        temp_tree, global_nodes = gaussian_tree.tree_decomposition([chain[-1][0] for chain in samples])
+        cl_tree = gaussian_tree.directed_graph(temp_tree)    
+        reference = lambda x: gaussian_tree.tree_logpdf(cl_tree, x, global_nodes)
         
         schedule = var_pt.update_schedule(reject_rates, schedule)
         curr_state = samples[-1]
@@ -80,6 +48,14 @@ def tree_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_target):
         
         ## Collect points for Lambda vs. r plot
         Lambda_vs_r_points_TREE.append([r, np.sum(reject_rates / (1 - reject_rates))])
+        
+        ## Collect points for NumRestarts vs. r plot
+        NumRestarts_vs_r_points.append([r, restarts])
+        
+        ## Collect points for Tau vs. r plot
+        RestartRate_vs_r_points.append([r, restarts/T])
     
-    return toReturn, reject_rates, Lambda_vs_r_points_TREE
+
+        
+    return toReturn, reject_rates, Lambda_vs_r_points_TREE, NumRestarts_vs_r_points, RestartRate_vs_r_points
 
--- code/var_pt.py
+++ code/var_pt.py
@@ -89,72 +89,88 @@ def update_diagonal_reference(samples):
     return (np.mean(samples, axis=0), np.diag(variances))
 
 
-def vanilla_NRPT_with_RWMH(initial_state, betas, log_annealing_path, num_iterations, variation):
+def vanilla_NRPT_with_RWMH(initial_state, betas, log_annealing_path, num_iterations):
     num_distributions = len(betas)
     d = 4
+    restarts = 0
 
     samples = []
     reject_rates = np.zeros(num_distributions-1)
     
     x_at_tminus1 = initial_state.copy()
-    x_at_t = np.zeros((num_distributions, d))
+    x_at_t = np.empty(num_distributions, dtype=object)
 
     for t in range(num_iterations):
         for init in range(num_distributions):
             log_gamma = log_annealing_path[init]
 
-            x_at_t[init] = RWMH_exploration_kernel(log_gamma, x_at_tminus1[init], 15)[-1]
+            curr_colour = x_at_tminus1[init][1]
+            x_at_t[init] = (RWMH_exploration_kernel(log_gamma, x_at_tminus1[init][0], 20)[-1], curr_colour)
         
         temp_alpha_vector = np.zeros(num_distributions)
 
         for i in range(num_distributions-1):
-            temp_alpha_vector[i] = nrpt.alpha(log_annealing_path[i], log_annealing_path[i+1], x_at_t[i], x_at_t[i+1])
+            temp_alpha_vector[i] = nrpt.alpha(log_annealing_path[i], log_annealing_path[i+1], x_at_t[i][0], x_at_t[i+1][0])
             reject_rates[i] = reject_rates[i] + (1 - temp_alpha_vector[i]) / num_iterations
             
             if ((i%2==0 and t%2==0) or (i%2==1 and t%2==1)) and (np.random.rand() <= temp_alpha_vector[i]):
                 x_at_t[i], x_at_t[i+1] = x_at_t[i+1], x_at_t[i]
+                
+                if i == num_distributions-2:
+                    if (x_at_t[i+1][1] == "b"):
+                        x_at_t[i+1] = (x_at_t[i+1][0], "w")
+                        restarts += 1
+                        
+                if i == 0:
+                    x_at_t[0] = (x_at_t[0][0], "b")
+                    x_at_t[1] = (x_at_t[1][0], "b")
+                
 
         x_at_tminus1 = x_at_t.copy()
         samples.append(x_at_tminus1.copy())
 
-    return {
+    return restarts, {
         "reject_rates": reject_rates,
         "samples": samples
-    }
+        }
     
     
 ## Variational PT implementation!
 def variational_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_target, log_var_family, initial_phi, diagonal, variation):
     schedule = np.linspace(0, 1, num_chains)
-    curr_state = initial_state
+    curr_state = [(point, "g") for point in initial_state]
     curr_phi = initial_phi
     
     Lambda_vs_r_points = []
+    RestartRate_vs_r_points = []
+    NumRestarts_vs_r_points = []
     
     toReturn = []
     
     for r in range(6, num_tuning_rounds):
         print("-----","TUNING ROUND", r,"-----")
         
+        curr_state = [(tup[0], "g") for tup in curr_state]
         T = 2**r
         
         curr_var = log_var_family(curr_phi[0], curr_phi[1])
         path = nrpt.path(schedule, curr_var, log_target)
         
-        result = vanilla_NRPT_with_RWMH(curr_state, schedule, path, T, variation)
+
+        restarts, result = vanilla_NRPT_with_RWMH(curr_state, schedule, path, T)
         reject_rates = result["reject_rates"]
         samples = result["samples"]
         
         if r >= 5: 
-            for chain in samples: toReturn.append(chain[-1])
+            for chain in samples: toReturn.append(chain[-1][0])
         
         schedule = update_schedule(reject_rates, schedule)
         
         if variation == True:
             if diagonal == True:
-                curr_phi = update_diagonal_reference([chain[-1] for chain in samples])
+                curr_phi = update_diagonal_reference([chain[-1][0] for chain in samples])
             else:
-                curr_phi = update_dense_reference([chain[-1] for chain in samples])
+                curr_phi = update_dense_reference([chain[-1][0] for chain in samples])
             
         curr_state = samples[-1]
         
@@ -164,5 +180,12 @@ def variational_PT_with_RWMH(initial_state, num_chains, num_tuning_rounds, log_t
         
         ## Collect points for Lambda vs. r plot
         Lambda_vs_r_points.append([r, np.sum(reject_rates / (1 - reject_rates))])
+        
+        ## Collect points for Num Restarts vs. r plot
+        NumRestarts_vs_r_points.append([r, restarts])
+        
+        ## Collect points for Tau vs. r plot
+        RestartRate_vs_r_points.append([r, restarts/T])
     
-    return toReturn, reject_rates, Lambda_vs_r_points
+    
+    return toReturn, reject_rates, Lambda_vs_r_points, NumRestarts_vs_r_points, RestartRate_vs_r_points
