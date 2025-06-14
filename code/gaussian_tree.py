3db3e934e1c75762091d2bebcc44506d9ea0ff86
Jack <therealjackwu@gmail.com>
2025-06-13 22:31:54 -0700

    Removed global, added vectorization optimizations

--- code/gaussian_tree.py
+++ code/gaussian_tree.py
@@ -11,38 +11,21 @@ warnings.filterwarnings("ignore")
 
 np.random.seed(1)
 
-global_nodes = []
-
 ## A variable in the dataset
 class Node:
     def __init__(self, label, data):
         self.label = label
-        self.data = data
+        self.data = np.array(data)
         self.edges = []
-
-
-## Fit a Gaussian to each variable
-def fit_gaussian(data):
-    data = np.asarray(data)
-    
-    mean = np.mean(data)
-    std = np.std(data)
-    
-    # mean = np.mean(data, axis=0)
-    # X = data - mean
-    # cov = (X.T @ X) / (X.shape[0]-1)
-    
-    return mean, std
+        self.mean = np.mean(data)
+        self.std = np.std(data)
 
 
 ## Compute mutual information weight
-def mutual_info(node1, node2):
-    X = np.asarray(node1.data)
-    Y = np.asarray(node2.data)
+# def mutual_info(node1, node2):
+    # rho = np.corrcoef(node1.data, node2.data)[0, 1]
     
-    rho = np.corrcoef(X, Y)[0, 1]
-    
-    return -0.5 * np.log(1 - rho**2)
+    # return -0.5 * np.log(1 - rho**2)
     
     # mean_XX, Sigma_XX = fit_gaussian(node1.data)
     # mean_YY, Sigma_YY = fit_gaussian(node2.data)
@@ -63,11 +46,13 @@ def mutual_info(node1, node2):
 
 ## Chow-Liu algorithm
 def tree_decomposition(samples):
-    global global_nodes
-    
     samples = np.asarray(samples)
+    
+    C = np.corrcoef(samples, rowvar=False)
+    W = -0.5 * np.log(1-C**2)
+    np.fill_diagonal(W,0)
+    
     nodes = [Node(str(i), np.array(list(samples[:,i]))) for i in range(samples.shape[1])]
-    global_nodes = nodes.copy()
     
     G = nx.Graph()
     
@@ -77,23 +62,14 @@ def tree_decomposition(samples):
             node1 = nodes[i]
             node2 = nodes[j]
             
-            G.add_edge(node1.label, node2.label, weight= -mutual_info(node1,node2))
+            G.add_edge(node1.label, node2.label, weight= W[i,j])
     
     edges = list(G.edges(data=True))
-    
-    # print()
-    # print("Num all edges:", len(edges))
-    # print("All edges:", edges)
-    # print()
     
     mst = nx.minimum_spanning_tree(G, algorithm='kruskal')
     edges = list(mst.edges(data=True))
     
-    # print("Num selected edges:", len(edges))
-    # print("Selected edges:", edges)
-    # print()
-    
-    return mst
+    return mst, nodes
 
 
 ## Generate 2D data vectors from an already Gaussian and already tree-like dependency structure:
@@ -129,15 +105,12 @@ def directed_graph(tree):
 
 ## Construct conditional pdf of two Normals: f(node1 = val1 | node2 = val2)
 def log_cond_gaussian(node1, node2, val1, val2):
-    X = np.asarray(node1.data)
-    Y = np.asarray(node2.data)
+    mean_X = node1.mean
+    mean_Y = node2.mean
+    var_X = (node1.std)**2
+    var_Y = (node2.std)**2
     
-    mean_X = np.mean(X)
-    mean_Y = np.mean(Y)
-    var_X = np.var(X)
-    var_Y = np.var(Y)
-    
-    rho = np.corrcoef(X,Y)[0,1]
+    rho = np.corrcoef(node1.data, node2.data)[0,1]
     
     new_mu = mean_X + rho * (np.sqrt(var_X) / np.sqrt(var_Y)) * (val2 - mean_Y)
     new_sigma = np.sqrt((1-rho**2) * var_X)
@@ -161,19 +134,17 @@ def log_cond_gaussian(node1, node2, val1, val2):
 
 
 ## Evaluate approximate log Chow-Liu joint density at input_x
-def tree_logpdf(directed_tree, input_x):
-    global global_nodes
-    
-    pdf = 0
-    temp1, temp2 = fit_gaussian(global_nodes[0].data)
-    # pdf *= multivariate_normal.pdf(input_x[0], temp1, temp2)
-    pdf += norm.logpdf(input_x[0], temp1, temp2)
+def tree_logpdf(directed_tree, input_x, global_nodes):
+    density = 0
+    node0 = global_nodes[0]
+    # density *= multivariate_normal.pdf(input_x[0], temp1, temp2)
+    density += norm.logpdf(input_x[0], node0.mean, node0.std)
     
     for edge in list(directed_tree.edges()):
         parent = int(edge[0])
         child = int(edge[1])
         
-        pdf += log_cond_gaussian(global_nodes[child], global_nodes[parent], input_x[child], input_x[parent])
+        density += log_cond_gaussian(global_nodes[child], global_nodes[parent], input_x[child], input_x[parent])
     
-    return pdf
+    return density
 
